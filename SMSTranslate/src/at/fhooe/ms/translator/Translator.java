3ed67b74e1c5a574331089f767fdecd943d5a0bd
viktorbaier <vibaier@gmail.com>
2015-01-20 12:08:50 +0100

    implemented automatic SMS Language detection and translation

--- SMSTranslate/src/at/fhooe/ms/translator/Translator.java
+++ SMSTranslate/src/at/fhooe/ms/translator/Translator.java
@@ -13,8 +13,8 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError; 
 import com.android.volley.toolbox.JsonObjectRequest;
-
-
+ 
+ 
 public class Translator extends Observable{
 
 	//private static final String YANDEX_USER="mcm.mobileservices:smstranslate";
@@ -22,8 +22,13 @@ public class Translator extends Observable{
 	private static final String YANDEX_URL_FIRST_PART="https://translate.yandex.net/api/v1.5/tr.json/translate?";
 	private static final String YANDEX_LANGUAGE="&lang=";
 	private static final String YANDEX_TEXT="&text=";
+	
+	private static final String YANDEX_LANGUAGE_REQUEST_URL="https://translate.yandex.net/api/v1.5/tr.json/detect?"+YANDEX_KEY+"&text=";
+	private static final int YANDEX_REQUEST_LANGUAGE=1;
+	private static final int YANDEX_REQUEST_TRANSLATION=2;
 	private Context mContext;
 	private String mTranslatedText;
+	private String outputLanguage;
 	/**
 	 * 
 	 */
@@ -38,17 +43,45 @@ public class Translator extends Observable{
 	 * @param _outputLanguage
 	 */
 	public void translate(String _textInput, String _inputLanguage, String _outputLanguage){
-		
-		String address=buildAddress(_textInput, _inputLanguage, _outputLanguage );
-		sendText(address);
+
+		String formatedText=formatText(_textInput);
+		outputLanguage=_outputLanguage;
+		if(_inputLanguage.contains(Languages.AUTOMATIC)){
+			String address = buildLanguageDetectionRequest(formatedText);
+			sendText(address, YANDEX_REQUEST_LANGUAGE);
+		}else{
+		String address= buildTranslationRequest(formatedText, _inputLanguage, _outputLanguage);
+		sendText(address, YANDEX_REQUEST_TRANSLATION);
+		}
 		
 	}
+	
 
 	/**
 	 * Sends text to the Yandex-server as a JSON Object request via HTTP. Notifies the observers, when the translated text is received.
 	 * @param _address http url that is send to the server.
 	 */
-	private void sendText(String _address) {
+	private void sendText(String _address, int _requestType ) {
+		JsonObjectRequest jsObjRequest;
+		if(_requestType== YANDEX_REQUEST_TRANSLATION){
+		 jsObjRequest=getTranslationRequest(_address);
+		}else{
+			jsObjRequest=getLanguageRequest(_address);
+		}
+		MySingleton.getInstance(mContext).addToRequestQueue(jsObjRequest);
+		RequestQueue queue=MySingleton.getInstance(mContext.getApplicationContext()).getRequestQueue();
+		queue.start();
+		
+		
+	}
+
+	/**
+	 * @param get
+	 * @param _address
+	 * @param object
+	 * @return
+	 */
+	private JsonObjectRequest getTranslationRequest(String _address) {
 		JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, _address, null, new Response.Listener<JSONObject>() {
 
 			@Override
@@ -72,11 +105,37 @@ public class Translator extends Observable{
 				Log.d("JSON_OUTPUT", arg0.getMessage());
 			}
 		});
-		MySingleton.getInstance(mContext).addToRequestQueue(jsObjRequest);
-		RequestQueue queue=MySingleton.getInstance(mContext.getApplicationContext()).getRequestQueue();
-		queue.start();
-		
+		return jsObjRequest;
 	}
+	
+	private JsonObjectRequest getLanguageRequest(final String _address) {
+		JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, _address, null, new Response.Listener<JSONObject>() {
+			String text=_address.replace(YANDEX_LANGUAGE_REQUEST_URL, "");
+			@Override
+			public void onResponse(JSONObject _response) {
+				try {
+					String a= _response.getString("lang").replace("\"", "");
+					String b= a.replace("\"", "");
+					String address= buildTranslationRequest(text, b, outputLanguage);
+					sendText(address, YANDEX_REQUEST_TRANSLATION);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				
+			}
+
+		}, new Response.ErrorListener() {
+
+			@Override
+			public void onErrorResponse(VolleyError arg0) {
+				Log.d("JSON_OUTPUT", arg0.getMessage());
+			}
+		});
+		return jsObjRequest;
+	}
+	
+	
+	
 
 	/**
 	 * Builds the http request url.
@@ -84,8 +143,7 @@ public class Translator extends Observable{
 	 * @param english original language
 	 * @param german target language
 	 */
-	private String buildAddress(String _textInput, String _input,
-			String _output) {
+	private String formatText(String _textInput) {
 		
 		String[] formatedTextArray=_textInput.split(" ");
 		String formatedText="";
@@ -96,9 +154,22 @@ public class Translator extends Observable{
 			}
 			formatedText+=formatedTextArray[i];
 		}
-		String address=YANDEX_URL_FIRST_PART+YANDEX_KEY+YANDEX_LANGUAGE+_input+"-"+_output +YANDEX_TEXT+formatedText;
-			return address; 
+		return formatedText;
+
+		
 	}
+	
+	private String buildTranslationRequest(String _formatedText, String _input, String _output){
+		return YANDEX_URL_FIRST_PART+YANDEX_KEY+YANDEX_LANGUAGE+_input+"-"+_output +YANDEX_TEXT+_formatedText;
+		
+	}
+	
+	private String buildLanguageDetectionRequest(String formatedText){
+		
+		return YANDEX_LANGUAGE_REQUEST_URL+formatedText;
+	}
+
+
 
 	/**
 	 * @return the translatedText
