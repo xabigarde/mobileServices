3c62bf0712214f75ae819a74e8188f37b86a417f
Xabier Gardeazabal <xgardeazabal@gmail.com>
2015-01-20 15:29:52 +0100

    Created new class: VoiceConverter.java 	Encapsulates the generation of a VXML file, the FTP upload, and the voice call

---
+++ SMSTranslate/src/at/fhooe/ms/voice/VoiceConverter.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package at.fhooe.ms.voice;
+
+import android.content.Context;
+
+/**
+ * 
+ * 
+ * @author Xabi
+ *
+ */
+public class VoiceConverter {
+	
+	private Context m_context;
+	
+	public VoiceConverter(Context _context){
+		m_context =_context;
+	}
+
+	public void generateVXML(String _inString){
+		
+	}
+	
+	public void uploadToFTPServer(){
+		
+	}
+	
+	public void makeAudioCall(){
+		
+	}
+	
+}
