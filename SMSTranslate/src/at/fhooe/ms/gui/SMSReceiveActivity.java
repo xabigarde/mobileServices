4d1228931f202671ff5f99adf9cf19ec00f46eaf
florianschweitzer <florian.schweitzer@tractive.com>
2015-01-20 16:06:51 +0100

    MOD translation, sms receive

--- SMSTranslate/src/at/fhooe/ms/gui/SMSReceiveActivity.java
+++ SMSTranslate/src/at/fhooe/ms/gui/SMSReceiveActivity.java
@@ -1,19 +1,35 @@
 package at.fhooe.ms.gui;
 
+import java.util.Observable;
+import java.util.Observer;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import at.fhooe.ms.smstranslate.R;
+import at.fhooe.ms.translator.Languages;
+import at.fhooe.ms.translator.Translator;
 
-public class SMSReceiveActivity extends Activity implements OnClickListener {
+public class SMSReceiveActivity extends Activity implements OnClickListener,
+		Observer {
 
 	private Button btnTranslate;
 	private Button btnHearIt;
+	private TextView tvSmsText;
+
+	private Translator translator;
+
+	private String lastReceivedMessage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +41,68 @@ public class SMSReceiveActivity extends Activity implements OnClickListener {
 
 		btnHearIt = (Button) findViewById(R.id.btnHearIt);
 		btnHearIt.setOnClickListener(this);
+
+		tvSmsText = (TextView) findViewById(R.id.txtReceivedSMS);
+
+		Log.i("smsreceive", "onCreate");
+
+		translator = new Translator(this);
+		translator.addObserver(this);
+
+		onSmsReceive(getIntent());
+	}
+
+	@Override
+	protected void onNewIntent(Intent i) {
+		super.onNewIntent(i);
+
+		Log.i("smsreceive", "onNewIntent");
+
+		onSmsReceive(i);
+	}
+
+	/**
+	 * @param i
+	 */
+	private void onSmsReceive(Intent intent) {
+		// Retrieves a map of extended data from the intent.
+		final Bundle bundle = intent.getExtras().getBundle("smsbundle");
+
+		try {
+
+			if (bundle != null) {
+
+				final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+				for (int i = 0; i < pdusObj.length; i++) {
+
+					SmsMessage currentMessage = SmsMessage
+							.createFromPdu((byte[]) pdusObj[i]);
+					String phoneNumber = currentMessage
+							.getDisplayOriginatingAddress();
+
+					String senderNum = phoneNumber;
+					lastReceivedMessage = currentMessage
+							.getDisplayMessageBody();
+
+					Log.i("SmsReceiver", "senderNum: " + senderNum
+							+ "; message: " + lastReceivedMessage);
+
+					// Show Alert
+					// int duration = Toast.LENGTH_LONG;
+					// Toast toast = Toast.makeText(this, "senderNum: "
+					// + senderNum + ", message: " + lastReceivedMessage,
+					// duration);
+					// toast.show();
+
+				} // end for loop
+			} // bundle is null
+
+		} catch (Exception e) {
+			Log.e("SmsReceiver", "Exception smsReceiver" + e);
+
+		}
+
 	}
 
 	@Override
@@ -56,11 +134,63 @@ public class SMSReceiveActivity extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btnTranslate:
+
+			showTranslationWaitScreen();
+
+			SharedPreferences pref = PreferenceManager
+					.getDefaultSharedPreferences(this);
+
+			String language = pref.getString("language_list", "0");
+			int languageIndex = Integer.parseInt(language);
+
+			String chosenLang = Languages.ENGLISH;
+
+			switch (languageIndex) {
+			case 0:
+				chosenLang = Languages.ENGLISH;
+				break;
+			case 1:
+				chosenLang = Languages.GERMAN;
+				break;
+			case 2:
+				chosenLang = Languages.FRENCH;
+				break;
+			case 3:
+				chosenLang = Languages.SPANISH;
+				break;
+			case 4:
+				chosenLang = Languages.RUSSIAN;
+				break;
+			}
+
+			translator.translate(lastReceivedMessage, Languages.AUTOMATIC,
+					chosenLang);
 			break;
 		case R.id.btnHearIt:
+
+			// TODO: call the generateXML + call initiation
 			break;
 		}
 
 	}
 
+	/**
+	 * 
+	 */
+	private void showTranslationWaitScreen() {
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+	 */
+	@Override
+	public void update(Observable arg0, Object data) {
+		Log.i("smsreceive", "update of observer: " + data.toString());
+
+		tvSmsText.setText(data.toString());
+
+	}
 }
